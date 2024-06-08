b9d469e405b81fb7b11a45c904b9739e71186750
lthoerner <lowellashton@gmail.com>
2024-06-08 09:46:03 -0700

    feat(config): add config model

--- src/main/java/com/lthoerner/betteranvilsreloaded/BetterAnvilsReloaded.java
+++ src/main/java/com/lthoerner/betteranvilsreloaded/BetterAnvilsReloaded.java
@@ -1,11 +1,12 @@
 package com.lthoerner.betteranvilsreloaded;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.lthoerner.betteranvilsreloaded.Config;
 
 public class BetterAnvilsReloaded implements ModInitializer {
+	public static final Config CONFIG = Config.createAndLoad();
     public static final Logger LOGGER = LoggerFactory.getLogger("betteranvils-reloaded");
 
 	@Override
---
+++ src/main/java/com/lthoerner/betteranvilsreloaded/ConfigModel.java
@@ -0,0 +1,52 @@
+package com.lthoerner.betteranvilsreloaded;
+
+import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Modmenu;
+
+@Modmenu(modId = "betteranvils-reloaded")
+@Config(name = "betteranvils-config", wrapperName = "Config")
+public class ConfigModel {
+    public boolean enableLevelOverrides = true;
+    public int protectionMaxLevel = 4;
+    public int fireProtectionMaxLevel = 4;
+    public int featherFallingMaxLevel = 4;
+    public int blastProtectionMaxLevel = 4;
+    public int projectileProtectionMaxLevel = 4;
+    public int respirationMaxLevel = 3;
+    public int aquaAffinityMaxLevel = 1;
+    public int thornsMaxLevel = 3;
+    public int depthStriderMaxLevel = 3;
+    public int frostWalkerMaxLevel = 2;
+    public int bindingCurseMaxLevel = 1;
+    public int soulSpeedMaxLevel = 3;
+    public int swiftSneakMaxLevel = 3;
+    public int sharpnessMaxLevel = 5;
+    public int smiteMaxLevel = 5;
+    public int baneOfArthropodsMaxLevel = 5;
+    public int knockbackMaxLevel = 2;
+    public int fireAspectMaxLevel = 2;
+    public int lootingMaxLevel = 3;
+    public int sweepingEdgeMaxLevel = 3;
+    public int efficiencyMaxLevel = 5;
+    public int silkTouchMaxLevel = 1;
+    public int unbreakingMaxLevel = 3;
+    public int fortuneMaxLevel = 3;
+    public int powerMaxLevel = 5;
+    public int punchMaxLevel = 2;
+    public int flameMaxLevel = 1;
+    public int infinityMaxLevel = 1;
+    public int luckOfTheSeaMaxLevel = 3;
+    public int lureMaxLevel = 3;
+    public int loyaltyMaxLevel = 3;
+    public int impalingMaxLevel = 5;
+    public int riptideMaxLevel = 3;
+    public int channelingMaxLevel = 1;
+    public int multishotMaxLevel = 1;
+    public int quickChargeMaxLevel = 3;
+    public int piercingMaxLevel = 4;
+    public int densityMaxLevel = 5;
+    public int breachMaxLevel = 4;
+    public int windBurstMaxLevel = 3;
+    public int mendingMaxLevel = 1;
+    public int vanishingCurseMaxLevel = 1;
+}
