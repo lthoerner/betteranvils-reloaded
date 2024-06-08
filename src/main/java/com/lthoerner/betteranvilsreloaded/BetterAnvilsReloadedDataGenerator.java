3f74d02b597c1e9c8a1550de9c93cfbf854f44d4
lthoerner <lowellashton@gmail.com>
2024-06-08 09:24:45 -0700

    initial commit

---
+++ src/main/java/com/lthoerner/betteranvilsreloaded/BetterAnvilsReloadedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lthoerner.betteranvilsreloaded;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterAnvilsReloadedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
